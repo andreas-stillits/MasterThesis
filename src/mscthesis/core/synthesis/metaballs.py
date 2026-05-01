fixed offset in porosity calculation on voxels

src/mscthesis/core/synthesis/metaballs.py
@@ -37,8 +37,10 @@ def _metadata(
     metadata["random_seed"] = random_seed
     metadata["plug_aspect"] = plug_aspect
     metadata["num_cells_placed"] = len(centers)
-    metadata["radius"] = float(np.mean(radii)) if len(radii) > 0 else 0.0
-    metadata["cell_volume"] = (
+    metadata["min_radius"] = float(np.min(radii)) if len(radii) > 0 else 0.0
+    metadata["mean_radius"] = float(np.mean(radii)) if len(radii) > 0 else 0.0
+    metadata["max_radius"] = float(np.max(radii)) if len(radii) > 0 else 0.0
+    metadata["mean_cell_volume"] = (
         float(np.mean([(4 / 3) * np.pi * r**3 for r in radii]))  # type: ignore
         if len(radii) > 0
         else 0.0
@@ -50,8 +52,16 @@ def _metadata(
     metadata["mesophyll_area_fraction"] = float(
         len(centers) * 4 * np.sum(radii**2) / plug_aspect**2
     )
-    metadata["type"] = "uniform"
+    metadata["type"] = "mixed"
     return metadata
+
+
+def _distance_field(
+    X: np.ndarray, Y: np.ndarray, Z: np.ndarray, center: np.ndarray, radius: float
+) -> np.ndarray:
+    return radius**2 / (
+        (X - center[0]) ** 2 + (Y - center[1]) ** 2 + (Z - center[2]) ** 2
+    )
 
 
 @log_call()
@@ -62,7 +72,9 @@ def generate_voxels_from_seed(
     separation: float,
     max_attempts: int,
     num_cells: int,
-    radius: float,
+    radius_min: float,
+    radius_max: float,
+    threshold: float,
 ) -> tuple[np.ndarray[tuple[int, int, int]], dict[str, Any]]:
     """
     Generate uniform swiss cheese voxel model for a given random seed.
@@ -74,7 +86,9 @@ def generate_voxels_from_seed(
         separation (float): Minimum separation distance between cells and boundaries.
         max_attempts (int): Maximum number of attempts to place each cell.
         num_cells (int): Number of cells (spheres) to place in the model.
-        radius (float): Radius of the cells (spheres).
+        radius_min (float): Minimum radius of the cells (spheres).
+        radius_max (float): Maximum radius of the cells (spheres).
+        threshold (float): Threshold for binarizing the voxel grid.
     Returns:
         np.ndarray: 3D numpy array of shape (planar_resolution, planar_resolution, resolution).
         dict[str, Any]: Metadata dictionary.
@@ -82,18 +96,18 @@ def generate_voxels_from_seed(
 
     rng = from_seed(random_seed).generator()
 
-    voxels, (X, Y, Z) = initialize_meshgrid(plug_aspect, resolution)
+    field, (X, Y, Z) = initialize_meshgrid(plug_aspect, resolution, dtype="float32")
 
     # initialize cell lists and determine placement boundaries
     centers = np.zeros((num_cells, 3))
     radii = np.zeros((num_cells,))
-    max_r = plug_aspect - radius - separation
-    min_z = radius + separation
-    max_z = 1 - radius - separation
+    max_r = plug_aspect - radius_min - separation
+    min_z = radius_min + separation
+    max_z = 1 - radius_min - separation
 
     if max_r <= 0:
         raise ValueError(
-            f"Cell size {radius} and separation {separation} too large for given plug aspect {plug_aspect}."
+            f"Cell size {radius_min} and separation {separation} too large for given plug aspect {plug_aspect}."
         )
 
     # placement of cells
@@ -109,29 +123,36 @@ def generate_voxels_from_seed(
                 ]
             )
 
+            # draw random cell radius
+            radius = rng.uniform(radius_min, radius_max)
+
             # enforce cyllindrical boundary
-            if np.linalg.norm(center[:2]) > max_r:
+            if np.linalg.norm(center[:2]) + radius - radius_min > max_r:
+                attempts += 1
+                continue
+            if (
+                center[2] + radius - radius_min > max_z
+                or center[2] - radius + radius_min < min_z
+            ):
                 attempts += 1
                 continue
 
-            # draw random cell radius and check for overlaps
+            # check for overlaps
             if np.all(
                 np.linalg.norm(centers[:i] - center, axis=1)
                 > (radii[:i] + radius + separation)
             ):
                 centers[i] = center
                 radii[i] = radius
+                field += _distance_field(X, Y, Z, center, radius)
                 break
             attempts += 1
 
         else:  # executed only if while loop is not stopped by break - then we dont attempt to place any further spheres
             break  # break out of the for loop
 
-        # compute distance field and update voxels
-        distance = np.sqrt(
-            (X - center[0]) ** 2 + (Y - center[1]) ** 2 + (Z - center[2]) ** 2
-        )
-        voxels |= (distance <= radius).astype(np.uint8)
+    # threshold the voxels to binary values
+    voxels = (field > threshold).astype(np.uint8)
 
     # remove cells where radius is still zero (not placed)
     centers = centers[radii > 0]
@@ -157,7 +178,9 @@ def generate_voxels_from_sample_id(
     separation: float,
     max_attempts: int,
     num_cells: int,
-    radius: float,
+    radius_min: float,
+    radius_max: float,
+    threshold: float,
 ) -> tuple[np.ndarray[tuple[int, int, int]], dict[str, Any]]:
     """
     Generate uniform swiss cheese voxel models for a given sample ID.
@@ -170,8 +193,9 @@ def generate_voxels_from_sample_id(
         separation (float): Minimum separation distance between cells and boundaries.
         max_attempts (int): Maximum attempts to place each cell without overlap.
         num_cells (int): Number of cells (spheres) to place in the model.
-        radius (float): Radius of the cells.
-
+        radius_min (float): Minimum radius of the cells.
+        radius_max (float): Maximum radius of the cells.
+        threshold (float): Threshold for binarizing the voxel grid.
     Returns:
         np.ndarray: 3D numpy array of shape (planar_resolution, planar_resolution, resolution)
         with uint8 values, where 1 indicates presence of tissue (cell)
@@ -189,7 +213,9 @@ def generate_voxels_from_sample_id(
         separation,
         max_attempts,
         num_cells,
-        radius,
+        radius_min,
+        radius_max,
+        threshold,
     )
 
     return voxels, metadata

src/mscthesis/core/synthesis/mixed.py
@@ -45,7 +45,7 @@ def _metadata(
         if len(radii) > 0
         else 0.0
     )
-    metadata["mean_porosity"] = 1.0 - float(np.sum(voxels) / voxels.size)
+    metadata["mean_porosity"] = 1.0 - float(np.sum(voxels) / (np.pi / 4 * voxels.size))
     metadata["std_porosity"] = float(
         np.std(1.0 - np.sum(voxels, axis=(0, 1)) / (voxels.shape[0] * voxels.shape[1]))
     )

src/mscthesis/core/synthesis/utils.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Literal
+
 import numpy as np
 
 from ...log import log_call
@@ -24,6 +26,7 @@ def get_sample_seed(base_seed: int, sample_id: str) -> int:
 def initialize_meshgrid(
     plug_aspect: float,
     resolution: int,
+    dtype: Literal["unit8", "float32"] = "unit8",
 ) -> tuple[np.ndarray[tuple[int, int, int]], tuple[np.ndarray, np.ndarray, np.ndarray]]:
     """
     Initialize a 3D meshgrid for voxel generation.
@@ -43,8 +46,11 @@ def initialize_meshgrid(
     X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
 
     # initialize empty voxels
+    datatype = (
+        np.uint8 if dtype == "unit8" else np.float32 if dtype == "float32" else None
+    )
     voxels = np.zeros(
-        (planar_resolution, planar_resolution, resolution), dtype=np.uint8
+        (planar_resolution, planar_resolution, resolution), dtype=datatype
     )
 
     return voxels, (X, Y, Z)